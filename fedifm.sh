84a009ec4c30c14128df02460ef9b25a2d228eec
NEETzsche <791672+TheStranjer@users.noreply.github.com>
2023-06-22 22:30:35 -0600

    Use JSON files

--- fedifm.sh
+++ fedifm.sh
@@ -1,7 +1,17 @@
-lastfm_api=`cat .lastfm_api.dat`
-lastfm_user=`cat .lastfm_user.dat`
-pleroma_instance=`cat .pleroma_instance.dat`
-pleroma_bearer=`cat .pleroma_bearer.dat`
+if [ `echo $1 | wc -m` -eq 1 ]
+then
+	echo "No config file submitted, aborting"
+	exit
+fi
+
+configfn=$1
+config=`cat $configfn`
+
+lastfm_api=`echo $config | jq -r '.lastfm_api'`
+lastfm_user=`echo $config | jq -r '.lastfm_user'`
+pleroma_instance=`echo $config | jq -r '.pleroma_instance'`
+pleroma_bearer=`echo $config | jq -r '.pleroma_bearer'`
+stored_total=`echo $config | jq -r '.total'`
 
 lastfm_api_result=`curl "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=$lastfm_user&api_key=$lastfm_api&format=json&limit=1"`
 
@@ -9,12 +19,14 @@ echo $lastfm_api_result > ~/example.json
 
 total=`echo $lastfm_api_result | jq -r '."recenttracks"."@attr".total'`
 
-stored_total=`cat .total.dat`
+function saveresults () {
+	jq -n --arg api "$lastfm_api" --arg user "$lastfm_user" --arg instance "$pleroma_instance" --arg bearer "$pleroma_bearer" --arg tot "$total" '{lastfm_api: $api, lastfm_user: $user, pleroma_instance: $instance, pleroma_bearer: $bearer, total: $tot|tonumber}' > $configfn
+}
 
-if [ -z $stored_total ]
+if [ "$stored_total" == "null" ]
 then
 	echo "No value yet. Storing total of $total for future use."
-	echo $total > .total.dat
+	saveresults
 	exit
 fi
 
@@ -41,5 +53,5 @@ curl -X POST "https://$pleroma_instance/api/v1/pleroma/scrobble" \
 
 echo "Submitted scrobble"
 
-echo $total > .total.dat
+saveresults
 
